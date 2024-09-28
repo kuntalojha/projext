27095e77ed536f6272d4cd6c66d3ac2d50ad9ac9
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-28 23:47:31 +0530

    set up nextjs

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <h1 className='text-6xl font-bold'>Next.js Hello World</h1>
+      <button className="text-6xl ">Hello</button>
+    </div>
+  );
+}
